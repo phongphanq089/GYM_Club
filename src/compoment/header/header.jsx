c7a05ae5015f637c16af3fd8d8ef0d85e555a706
phong <phongphanq089@gmail.com>
2022-09-29 19:25:27 +0700

    hoan thanh du an

--- src/compoment/header/header.jsx
+++ src/compoment/header/header.jsx
@@ -1,36 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./header.scss"
 import logo from "../../accsets/images/logo.png"
 import { Link } from "react-scroll"
+import {AiOutlineBars,AiFillCloseCircle} from "react-icons/ai"
+import { FlatTree } from 'framer-motion'
 const Header = () => {
+    const [openMobile, setOpenmobile] = useState(true)
     return (
         <div className="header" id='home'>
-            <img src={logo} alt='' className='logo' />
-
+             <img src={logo} alt='' className='logo' />
+           {openMobile === false ?
+          ( <div className='bar'onClick={() => setOpenmobile(true)}><AiOutlineBars /></div>)
+           :
+           (
             <ul className="header-menu">
-                <li>Home</li>
-                <li><Link
-                    to='program'
-                    span={true}
-                    smooth={true}
-                >Programs</Link>
-                </li>
-                <li><Link
-                    to='reasons'
-                    span={true}
-                    smooth={true}
-                >Why Us</Link></li>
-                <li><Link
-                    to='plans'
-                    span={true}
-                    smooth={true}
-                >Plans</Link></li>
-                <li><Link
-                    to='testimonials'
-                    span={true}
-                    smooth={true}
-                >Testimonials</Link></li>
-            </ul>
+            <li><Link
+                to='home'
+                span={true}
+                smooth={true}
+            >Home</Link></li>
+            <li><Link
+                to='program'
+                span={true}
+                smooth={true}
+            >Programs</Link>
+            </li>
+            <li><Link
+                to='reasons'
+                span={true}
+                smooth={true}
+            >Why Us</Link></li>
+            <li><Link
+                to='plans'
+                span={true}
+                smooth={true}
+            >Plans</Link></li>
+            <li><Link
+                to='testimonials'
+                span={true}
+                smooth={true}
+            >Testimonials</Link></li>
+         <div className="icon-close" onClick={()=> setOpenmobile(false)}><AiFillCloseCircle/></div>
+        </ul>   
+         
+           )
+          }
+         
         </div>
     )
 }
